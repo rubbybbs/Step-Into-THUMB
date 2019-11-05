0923787ae8b9ffb951489b0ce1c200592f5a15e2
mzp0514 <mzp0514@gmail.com>
2019-11-05 20:04:40 +0800

    数据库改动 部分后端接口

--- SITHUMB/models.py
+++ SITHUMB/models.py
@@ -5,14 +5,25 @@ from django.db.models.signals import post_save
 
 
 class Activity(models.Model):
-    name = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+    section_cnt = models.IntegerField(default=0)
+    name = models.CharField(max_length=100, default="")
     application_format = models.TextField()
-    transcript_format = models.TextField()
+
+
+class Section(models.Model):
+    s_id = models.IntegerField()
+    name = models.CharField(max_length=100, default="")
+    from_date = models.DateField(default=None)
+    to_date = models.DateField(default=None)
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, related_name='sections')
+    transcript_format = models.TextField(default="")
 
 
 class Examiner(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    activity = models.ManyToManyField(Activity)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='extension')
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, related_name='examiners')
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, related_name='examiners')
 
 
 @receiver(post_save, sender=User)
@@ -24,19 +35,18 @@ def handler_user_extension(sender, instance, created, **kwargs):
 
 
 class Candidate(models.Model):
-    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, related_name='candidates')
     examiners = models.ManyToManyField(Examiner)
     stage = models.IntegerField()
 
 
 class Application(models.Model):
-    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
-    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
-    form = models.TextField()
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, related_name='applications')
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE, related_name='application')
+    form = models.TextField(default="")
 
 
 class Transcript(models.Model):
-    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
-    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
-    form = models.TextField()
-
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, related_name='transcripts')
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE, related_name='transcript')
+    form = models.TextField(default="")
--- SITHUMB/views.py
+++ SITHUMB/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import render
+from django.utils.dateparse import parse_date
+from django.contrib.auth.models import *
+from .models import *
+import json
+
+
+def create_examiner_view(request):
+    activity_id = request.GET.get('id')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    examiner = User(username=username, password=make_password(password))
+    examiner.extension.activity = Activity.objects.get(id=activity_id)
+    examiner.save()
+    # return
+
+
+def create_section_view(request):
+    activity_id = request.GET.get('id')
+    name = request.POST.get('name')
+    from_date = parse_date(request.POST.get('from'))
+    to_date = parse_date(request.POST.get('to'))
+    ac = Activity.objects.get(id=activity_id)
+    section = Section(s_id=ac.section_cnt, name=name,
+                      from_date=from_date, to_date=to_date)
+    ac.section_cnt += 1
+    ac.save()
+    section.save()
+
+
+def add_examiner_view(request):
+    activity_id = request.GET.get('id')
+    section_id = request.GET.get('part-id')
+    username = request.POST.get('username')
+    examiner = User.objects.get(username=username).extension
+    activity = Activity.objects.get(id=activity_id)
+    examiner.section = Section.objects.get(s_id=section_id, activity=activity)
+    examiner.save()
+
+
+def create_form_view(request):
+    activity_id = request.GET.get('id')
+    section_id = request.GET.get('part-id')
+    transcript_format = json.dumps(request.body)
+    activity = Activity.objects.get(id=activity_id)
+    section = Section.objects.get(s_id=section_id, activity=activity)
+    section.transcript_format = transcript_format
+    section.save()
