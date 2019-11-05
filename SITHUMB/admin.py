561d0a8a0a31692d75203a92510492e4fd9cb676
rubby <rubbybbs1324@gmail.com>
2019-11-05 21:34:54 +0800

    bug fix

--- SITHUMB/admin.py
+++ SITHUMB/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Activity, Examiner, Candidate, Transcript, Application, Section
 # Register your models here.
+admin.site.register(Activity)
+admin.site.register(Examiner)
+admin.site.register(Candidate)
+admin.site.register(Transcript)
+admin.site.register(Application)
+admin.site.register(Section)
