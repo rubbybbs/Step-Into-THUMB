863a202b73f52a8b200298f4f6e4bb5dda220d84
rubbybbs <rubbybbs1324@gmail.com>
2020-01-07 20:22:50 +0800

    认证格式统一

--- SITHUMB/authentication_module.py
+++ SITHUMB/authentication_module.py
@@ -1,5 +1,6 @@
 from rest_framework.authentication import BaseAuthentication
 from SITHUMB import models
+from SITHUMB import views
 from rest_framework.exceptions import NotAuthenticated
 from SITHUMB.token_module import get_token, out_token
 from django.shortcuts import redirect
@@ -7,9 +8,9 @@ from django.shortcuts import redirect
 class TokenAuth2(BaseAuthentication):
     def authenticate(self, request):
         token = request.GET.get("token")
-        name = request.GET.get("username")
+        name = request.GET.get("uname")
         token_obj = out_token(name, token)
         if token_obj:
             return
         else:
-            raise NotAuthenticated("你没有登入")
+            raise NotAuthenticated("You need login!")
